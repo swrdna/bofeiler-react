e21adc684a6a04e917e77a6bd755a95705285abe
Nano <swrdna@hotmail.com>
2024-12-14 15:59:25 -0800

    Add dependencies

--- farm.config.ts
+++ farm.config.ts
@@ -1,5 +1,25 @@
-import { defineConfig } from '@farmfe/core';
+import path from 'path';
+import { defineConfig, loadEnv } from '@farmfe/core';
+import farmJsPluginSvgr from '@farmfe/js-plugin-svgr';
 
-export default defineConfig({
-  plugins: ['@farmfe/plugin-react']
-});
+interface IConfigArgs {
+  mode: 'development' | 'test' | 'production';
+}
+
+export default ({ mode }: IConfigArgs) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: ['@farmfe/plugin-react', farmJsPluginSvgr()],
+    compilation: {
+      sourcemap: mode !== 'production',
+      resolve: {
+        alias: { '@': path.join(process.cwd(), 'src') },
+      },
+    },
+    server: {
+      port: Number(env.FARM_SERVER_PORT),
+      host: Boolean(env.FARM_SERVER_HOST === 'true'),
+    },
+  });
+};
