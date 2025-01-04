0230f3bf3ed95ce35baa453deadb19f513936959
Nano <swrdna@hotmail.com>
2025-01-04 09:18:22 +0800

    Update login form card

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/features';
 
 const LoginPage = () => (
   <div className="flex h-screen w-full items-center justify-center px-4">
-    <div className="card bg-base-300 shadow-xl">
+    <div className="card border">
       <div className="card-body">
         <div className="card-title justify-center mb-4">Bofeiler</div>
         <LoginForm />
